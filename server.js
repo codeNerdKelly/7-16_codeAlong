fdc41555588d6526827f02da3559b811290a2765
codeNerdKelly <48159112+codeNerdKelly@users.noreply.github.com>
2022-07-21 19:20:00 -0400

    adding conditions for server requests

--- server.js
+++ server.js
@@ -2,7 +2,40 @@ console.log('hello world')
 
 const http = require('http');
 
-const server = http.createServer();
+const server = http.createServer((req, res)=>{
+    if(req.url ==='/'){
+        res.write(`
+        <html>
+          <head>
+            <title>Home</title>
+          </head>
+          <body>
+            <a href = '/' class = 'selected'>Home</a>
+            <a href = '/products'>Products</a>
+            <a href = '/faqs'>FAQs/a>
+            </nav>
+           </body>
+           </html> 
+        `)
+    }
+    else  if(req.url ==='/products'){
+        res.write(`
+        <html>
+          <head>
+            <title>Home</title>
+          </head>
+          <body>
+            <a href = '/' class = 'selected'>Home</a>
+            <a href = '/'>Products</a>
+            <a href = '/'>FAQs/a>
+            </nav>
+           </body>
+           </html> 
+        `)
+    }
+    res.write('hello world');
+    res.end();
+});
 
 
 const port = process.env.PORT || 3000;
