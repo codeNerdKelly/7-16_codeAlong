e709bb6c5f7b131809b1db9075f22e3b8eed9c96
codeNerdKelly <48159112+codeNerdKelly@users.noreply.github.com>
2022-07-21 19:04:56 -0400

    adding server code

--- server.js
+++ server.js
@@ -3,3 +3,10 @@ console.log('hello world')
 const http = require('http');
 
 const server = http.createServer();
+
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, ()=>{
+    console.log(`listening on port ${port}`)
+});
